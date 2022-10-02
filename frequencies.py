33d0fd5cd5f0317b6c4e14b3372a2b3720d9335e
Melane Navaratnarajah <melane.navaratnarajah@kcl.ac.uk>
2022-10-02 22:02:59 +0100

    Add functionality to count for unique values

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,14 @@
 
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+    items_str = list(map(str, items))
+          ##Changes each element to a string
+    for i in items_str:
+        count = 0
+        count = items_str.count(i)
+
+        if i in frequencies.keys():
+            pass ##Checks if key is already exists
+        else:
+            frequencies[i] = count
     return frequencies
